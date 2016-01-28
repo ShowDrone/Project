620be3693a3825375f74b9e8369171d4baa95a89
ShowDrone <seunghy0610@gmail.com>
2016-01-28 13:46:07 +0900

    1.00

---
+++ Xydro_Project/Test/moving_average_filter_tx/moving_average_filter_tx.ino
@@ -0,0 +1,56 @@
+//Maker : Seunghyun Lee http://dkwltmdgus.blog.me
+//공유하실 땐 출처 남겨주세요~
+
+#include "Wire.h"
+#include "I2Cdev.h"
+#include "MPU9150.h"
+#include <AltSoftSerial.h>
+AltSoftSerial altSerial;
+
+MPU9150 accelGyroMag;
+
+int16_t AcX, AcY, AcZ;
+int16_t GyX, GyY, GyZ;
+int16_t MgX, MgY, MgZ;
+
+double rollGyro, pitchGyro, yawGyro;
+double rollAcc, pitchAcc, yawAcc;
+double pitch = 0, roll = 0, yaw = 0;
+float DT = 0.01;
+
+void setup() {
+  Serial.begin(19200);
+  altSerial.begin(19200);
+  Wire.begin();
+  accelGyroMag.initialize();
+}
+int time_pre = 0;
+void loop() {
+  int time1 = millis() - time_pre;
+  time_pre = millis();
+  Serial.println(time1);
+
+  accelGyroMag.getMotion6(&AcX, &AcY, &AcZ, &GyX, &GyY, &GyZ);
+  //가속도 계산
+  pitchAcc = atan2(AcY, AcZ) * 180 / PI; // 라디안 to 각도값
+  rollAcc = -atan2(AcX, AcZ) * 180 / PI;
+  yawAcc = atan2(AcX, AcY) * 180 / PI;
+
+  //자이로 계산
+  pitchGyro = GyX / 131.07; // 16비트 값 to 250'/sec
+  rollGyro = GyY / 131.07;
+  yawGyro = GyZ / 131.07;
+
+  //상보필터
+  pitch = (0.98 * (pitch + (pitchGyro * DT))) + (0.02 * pitchAcc);
+  roll = (0.98 * (roll + (rollGyro * DT))) + (0.02 * rollAcc);
+  yaw = (0.98 * (yaw + (yawGyro * DT))) + (0.02 * rollAcc);
+  
+  altSerial.write(roll);
+  altSerial.write(pitch);
+  altSerial.write(yaw);
+  
+  //Serial.print("  pitch: "); Serial.print(pitch);
+  //Serial.print(" roll: "); Serial.print(roll);
+  //Serial.print(" yaw: "); Serial.println(yaw);
+}
