384056ce854f851a2b2b8e3fe85009186173df24
ShowDrone <seunghy0610@gmail.com>
2016-02-02 11:10:27 +0900

    1.08
    Transmitter Uart adjust

--- Pjt_Xydro/Real/Xydro_ArduinoB/Status_UART.ino
+++ Pjt_Xydro/Real/Xydro_ArduinoB/Status_UART.ino
@@ -15,25 +15,28 @@ void Transmiter_Update() {
   if (rollCheck) {
     if (waitTime + 3 > millis()) {
       altSerial.write(roll);
-      waitTime = millis();
       rollCheck = false;
       pitchCheck = true;
+      Serial.print(" 1 ");
+      waitTime = millis();
     }
   }
   else if (pitchCheck) {
     if (waitTime + 3 > millis()) {
       altSerial.write(pitch);
-      waitTime = millis();
       pitchCheck = false;
       yawCheck = true;
+      Serial.print(" 2 ");
+      waitTime = millis();
     }
   }
   else if (yawCheck) {
     if (waitTime + 3 > millis()) {
       altSerial.write(yaw);
-      waitTime = millis();
       yawCheck = false;
       rollCheck = true;
+      waitTime = millis();
+      Serial.println(" 3 ");
     }
   }
 }
