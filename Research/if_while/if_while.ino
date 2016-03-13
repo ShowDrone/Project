8cb79d0be4e3aaf08fd9a64b1387c2a74e2c4c52
ShowDrone <seunghy0610@gmail.com>
2016-03-13 13:33:12 +0900

    1.00

--- Research/if_while/if_while.ino
+++ Research/if_while/if_while.ino
@@ -6,17 +6,30 @@ void setup() {
   mySerial.begin(19200);
 }
 
+int time = 0;
 int pre_time = 0;
-int sum = 0;
-void loop() {
-  int time = micros() - pre_time;
-  while (mySerial.available()) {
-    int check = mySerial.read();
-  }
-  Serial.println(time);
+int check = 0;
+int value = 0;
+int32_t value_sum = 0;
+boolean value_check = 0;
 
-  if(time > 100) {
-  }
-  
+void loop() {
+  time = micros() - pre_time;
   pre_time = micros();
+  while (mySerial.available()) { // 이부분만 while로 바꿈
+    check = mySerial.read();
+  }
+
+  if (value < 1000) {
+    value_sum += time;
+    value++;
+  }
+
+  if (value == 1000) {
+    Serial.println(value_sum / value);
+    value_sum = 0;
+    value = 0;
+    delay(3000);
+  }
+  Serial.print(value); Serial.print(" "); Serial.println(time);
 }
